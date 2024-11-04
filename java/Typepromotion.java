eca0626b0095beb794a67df188bd3f277ff246cd
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-05 05:42:19 +0545

    type promotion_casting

---
+++ java/Typepromotion.java
@@ -0,0 +1,30 @@
+
+public class Typepromotion {
+            public static void main(String args[]) {
+                        // int a = 257;
+                        // byte b = (byte) (a);
+                        // System.out.println("Byte : " + b);
+
+
+                        // byte a = 10 ;
+                        // byte b = 30 , c = 40 ; 
+                        // int result = a+b+c ;
+
+                        int milion = 323_000_000 ; 
+                        
+
+                        byte b = 42 ; 
+                        char c = 'a' ; 
+                        short s= 1024 ; 
+                        int i = 5000 ; 
+                        float f  = 3.14f ;
+                        double d = 0.12323 ;
+                        double result = (f*b) + (i/c) +(d*s) ; 
+                        // flaot + int +double = double 
+                        // hence the result is a double value */
+
+            }
+}
+
+
+//* Some rules of tyep promotion (1) smaller data type char , byte , short into int  */
