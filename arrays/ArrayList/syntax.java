9e65ba46d4f1a8d31c9e4b5b46ca735ad14bcdb7
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-07 12:58:46 +0545

    Arraylist

---
+++ arrays/ArrayList/syntax.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class syntax {
+            public static void main(String[] args) {
+                        ArrayList<String> cars = new ArrayList<String>();
+
+                        // add items
+                        cars.add("volvo");
+                        cars.add("bmw");
+                        cars.add("mercedes");
+
+                        System.out.println(cars);
+
+                        // get items
+                        String mycar = cars.get(0);
+
+                        // modify an item
+                        cars.set(1, "modifiec car");
+
+                        // size
+                        int size = cars.size();
+
+                        // implement loops
+                        for (int i = 0; i < cars.size(); i++) {
+                                    System.out.print(cars.get(i) + " ") ; 
+                        }
+
+
+                        // remove element
+                        cars.remove(0); // removes element at first index
+                        cars.clear(); // clears all elements
+            }
+}
