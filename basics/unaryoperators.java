c010a09e7a6b55a5cadf58cd59b902167bb7bf7e
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-07 06:53:18 +0545

    core java

--- basics/unaryoperators.java
+++
@@ -1,7 +0,0 @@
-public class unaryoperators {
-       public static void main(String[] args){
-            int a = 10 ; 
-            int b = ++a ; 
-            System.out.println(b) ;
-       }
-}
