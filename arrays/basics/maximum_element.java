8230fc3a3df392c7390ba7f3ad94b6644ef5632d
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-10 04:39:49 +0545

    add problem

--- arrays/basics/maximum_element.java
+++ arrays/basics/maximum_element.java
@@ -1,10 +1,11 @@
 import java.util.* ; 
+//Maximum item in a range 
 
-public class largestelement{
+public class maximum_element{
 
-            public static int getLargest(int[] numbers){
-                        int largest = Integer.MIN_VALUE ;
-                        for ( int i = 0 ; i < numbers.length ; i++){
+            public static int getLargest(int[] numbers , int a , int b ){
+                        int largest = Integer.MIN_VALUE ;  //works even if array is empty 
+                        for ( int i = a ; i < b ; i++){
                                     if(largest < numbers[i]){
                                                 largest = numbers[i] ; 
                                     }
@@ -14,7 +15,7 @@ public class largestelement{
 
             public static void main(String[] args){
                         int[] numbers = {4 , 5 , 5, 323 , 54 ,-2} ; 
-                        int largest = getLargest(numbers) ;
+                        int largest = getLargest(numbers , 0 , 2) ;
                         System.out.println("The largest element is " + largest);
             }
 }
--- arrays/basics/syntaxarray.java
+++ arrays/basics/syntaxarray.java
@@ -1,4 +1,11 @@
+import java.util.* ; 
+
 public class syntaxarray {
+
+            public static void changearray ( int [] marks ) {
+                        marks[0] = 10 ; 
+            }
+
             public static void main(String[] args) {
                         int[] numbers; // declaration of aray , numbers is getting defined in stack
                         numbers = new int[5]; // here actaully object is being created in heap memory
@@ -6,6 +13,23 @@ public class syntaxarray {
                         int marks[] = {12,3,234,423};
                         System.out.println(marks[3]);
 
+                        //for each loop 
+                        for ( int  ans : marks ){
+                                    System.out.print(ans + " ");
+                        }
+
+
+                       
+
+
+
+                        //Array of objects 
+                        String[] str  = new String[5] ; 
+                        
+
+                        changearray(marks) ; 
+
+                        System.out.println(Arrays.toString(marks)) ;
 
             }
 }
