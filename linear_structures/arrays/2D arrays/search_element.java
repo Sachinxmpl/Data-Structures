edf04f16cd1cceb211bb80296c533462dcc30a7d
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-10 06:53:17 +0545

    add problem

---
+++ linear_structures/arrays/2D arrays/search_element.java
@@ -0,0 +1,29 @@
+public class search_element {
+            public static void main(String[] args) {
+                        int arr[][] = {
+                                                { 1, 2, 65, 45 },
+                                                { 234, 4643, 23, 3 },
+                                                { 234, 34, 6 }
+                        };
+                        int target = 234;
+                        System.out.println("The position of target element is" + search(arr, target)[0] + ","
+                                                + search(arr, target)[1]);
+
+            }
+
+            static int[] search(int[][] arr, int target) {
+                        
+                        if (arr.length == 0) {
+                                    return new int[]{-1 , -1};
+                        }
+                        for (int i = 0; i < arr.length; i++) {
+                                    for (int j = 0; j < arr[i].length; j++) {
+                                                if (arr[i][j] == target) {
+                                                           return new int[]{i,j};
+                                                }
+                                    }
+                        }
+
+                        return new int[]{-1 , -1};
+            }
+}
---
+++ linear_structures/strings/basics/search_character.java
@@ -0,0 +1,19 @@
+public class search_character {
+            public static void main(String[] args) {
+                        String name = "Sachin";
+                        char target = 'n';
+                        System.out.println(linearSearch(name, target));
+            }
+
+            static char linearSearch(String str, char ch) {
+                        if (str.length() == 0) {
+                                    return '!';
+                        }
+                        for (int i = 0; i < str.length(); ++i) {
+                                    if (ch == str.charAt(i)) {
+                                                return ch;
+                                    }
+                        }
+                        return '!';
+            }
+}
