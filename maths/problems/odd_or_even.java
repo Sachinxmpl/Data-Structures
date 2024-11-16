6471001289271b2aff0fd910f2dca1af20bac42c
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-16 12:12:17 +0545

    add problem

---
+++ maths/problems/odd_or_even.java
@@ -0,0 +1,12 @@
+package maths.problems;
+
+public class odd_or_even {
+        public static void main(String[] args) {
+                int a= 3 ; 
+                if((a & 1 )== 0 ){
+                        System.out.println("Even");
+                }else{
+                        System.out.println("odd");
+                }
+        }
+}
