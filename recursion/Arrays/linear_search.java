04b5af698ea921f41eaf871c19682a3a211abccc
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-22 08:58:58 +0545

    add problem

---
+++ recursion/Arrays/linear_search.java
@@ -0,0 +1,20 @@
+package Data_Structures_and_algorithms.recursion.Arrays;
+
+public class linear_search {
+    public static void main(String[] args) {
+        int[] nums = {23,5,7,23,78,6};
+        System.out.println(Search(nums, 0, 6));
+    }
+
+    static int Search(int[] nums , int index , int target){
+        if(index == nums.length){
+            return -1;
+        }
+        if(nums[index] == target){
+            return index;
+        }
+        return Search(nums, index+1, target);
+    }
+}
+
+//time and spaze O(N)
