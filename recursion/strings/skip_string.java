eae8b256ee819fc597e5c002f2278d10a824d98a
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-23 10:37:17 +0545

    update

---
+++ recursion/strings/skip_string.java
@@ -0,0 +1,38 @@
+package Data_Structures_and_algorithms.recursion.strings;
+
+public class skip_string {
+    public static void main(String[] args) {
+        String str = "Apple is eaten by Sapple eater";
+        skip("",str);
+
+        String ans = skip(str);
+        System.out.println(ans);
+    }
+    
+    static String skip(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+        char ch = up.charAt(0);
+        if(up.startsWith("Apple") || up.startsWith("apple")){
+            return skip( up.substring("Apple".length()));
+        }else{
+            return ch + skip( up.substring(1));
+        }
+    }
+
+    //skip string apple 
+    static void skip(String p , String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return ; 
+        }
+        char ch = up.charAt(0);
+        if(up.startsWith("Apple") || up.startsWith("apple")){
+            skip(p, up.substring("Apple".length()));
+        }else{
+            skip(p+ch, up.substring(1));
+        }
+
+    }
+}
