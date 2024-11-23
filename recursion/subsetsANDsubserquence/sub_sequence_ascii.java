16faa4864d6ea6c1f645be56c94231d53687f9a3
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-23 12:18:25 +0545

    add problem

---
+++ recursion/subsetsANDsubserquence/sub_sequence_ascii.java
@@ -0,0 +1,19 @@
+package Data_Structures_and_algorithms.recursion.subsetsANDsubserquence;
+
+public class sub_sequence_ascii {
+    public static void main(String[] args) {
+        String str= "abc";  
+        findSubset("", str);  
+    }
+    static void findSubset(String p , String up){
+        if(up.isEmpty()){
+             System.out.println(p);
+             return;
+        }
+        char ch = up.charAt(0);
+
+        findSubset(p+ch, up.substring(1));
+        findSubset(p, up.substring(1));
+        findSubset(p+(ch+0), up.substring(1));
+    }
+}
