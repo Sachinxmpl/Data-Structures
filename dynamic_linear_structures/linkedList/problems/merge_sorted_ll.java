af719730aa82f024a410fde9916db38f3ce2f490
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-25 21:19:54 +0545

    add problem

--- dynamic_linear_structures/linkedList/problems/merge_sorted_ll.java
+++ dynamic_linear_structures/linkedList/problems/merge_sorted_ll.java
@@ -38,3 +38,34 @@ class Solution {
         return dummyHead.next;
     }
 }
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode();
+        ListNode head = temp;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                temp = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                temp = list2;
+                list2 = list2.next;
+            }
+        }
+
+        while (list1 != null) {
+            temp.next = list1;
+            temp = list1;
+            list1 = list1.next;
+        }
+        while (list2 != null) {
+            temp.next = list2;
+            temp = list2;
+            list2 = list2.next;
+        }
+
+        return head.next;
+    }
+}
