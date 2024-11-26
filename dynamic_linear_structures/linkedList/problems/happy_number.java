84bcbfcabd500e03ca5c9c2caaa54a6361e968a6
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-26 09:57:18 +0545

    add problem

---
+++ dynamic_linear_structures/linkedList/problems/happy_number.java
@@ -0,0 +1,30 @@
+package dynamic_linear_structures.linkedList.problems;
+
+public class happy_number {
+    
+}
+//https://leetcode.com/problems/happy-number/description/
+
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n ; 
+        int fast = n ;
+        do{
+            slow = findSquareODigits(slow);
+            fast = findSquareODigits(findSquareODigits(fast));
+        } while(fast != slow);
+        if(slow == 1){
+            return true ; 
+        }
+        return false ; 
+    }
+    int findSquareODigits(int n ){
+        int sum = 0 ; 
+        while( n > 0){
+            int rem  = n % 10 ; 
+            sum = sum + rem * rem ; 
+            n = n / 10 ; 
+        }
+        return sum ; 
+    }
+}
