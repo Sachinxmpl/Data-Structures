3fa9c6baad0877bf557431beae63300d1dd93260
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-29 18:15:57 +0545

    add problem

--- recursion/permutations/permutation_string.java
+++ recursion/permutations/permutation_string.java
@@ -1,8 +1,14 @@
-package Data_Structures_and_algorithms.recursion.permutations;
+package recursion.permutations;
+
+import java.util.ArrayList;
 
 public class permutation_string {
     public static void main(String[] args) {
         permuations("","abc");
+
+        ArrayList<String> ans = new ArrayList<String>();
+        helper2("", "abcd", ans);
+        System.out.println(ans.toString());
     }
 
     static void permuations(String p , String up){
@@ -11,10 +17,39 @@ public class permutation_string {
             return ; 
         }
         char ch = up.charAt(0);
-        for ( int i = 0 ; i < p.length()+1 ; i++){
+        for ( int i = 0 ; i <= p.length() ; i++){
               String f = p.substring(0,i);
               String l = p.substring(i,p.length());
               permuations(f+ch+l, up.substring(1));
         }
+    }
+
+
+    static void helper2(String p , String up , ArrayList<String> list){
+        if(up.isEmpty()){
+            list.add(p);
+            return ; 
+        }
+        char ch = up.charAt(0);
+        for ( int i = 0 ; i <= p.length() ; i++){
+            String f = p.substring(0,i);
+            String l = p.substring(i, p.length());
+            helper2(f+ch+l, up.substring(1), list);
+        }
+    }
+
+
+    static int permuationsCount(String p , String up){
+        if(up.isEmpty()){
+            return 1; 
+        }
+        int count = 0 ; 
+        char ch = up.charAt(0);
+        for ( int i = 0 ; i <= p.length() ; i++){
+              String f = p.substring(0,i);
+              String l = p.substring(i,p.length());
+              count = count + permuationsCount(f+ch+l, up.substring(1));
+        }
+        return count;
     }
 }
