9b7b05226008f883dd46e882e2421fb018207864
Sachinxmpl <sachinxmpl6@gmail.com>
2024-11-30 16:41:58 +0545

    add problem

---
+++ recursion/mazeProblems/allPaths.java
@@ -0,0 +1,23 @@
+package recursion.mazeProblems;
+
+public class allPaths {
+    public static void main(String[] args) {
+        maze("",0,0,2,2);
+    }    
+    static void maze(String p , int row , int col , int d1 , int d2){
+        if(row == d1 && col == d2){
+            System.out.println(p);
+            return ; 
+        }
+        if(row == d1){
+            maze(p + "r", row , col+1 , d1 ,d2);
+        }
+        else if(col == d2){
+            maze(p+"d" , row+1 , col , d1, d2);
+        }
+        else {
+            maze(p + "r", row , col+1 , d1 ,d2);
+            maze(p+"d" , row+1 , col , d1, d2);
+        }
+    }
+}
