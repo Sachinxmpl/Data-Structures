fc66e456cfe97ad62cc0724b6b4b5bf442f8148f
Sachinxmpl <sachinxmpl6@gmail.com>
2024-12-02 20:45:13 +0545

    add problem

---
+++ non_linear_dataStructures/binary_tree/main.java
@@ -0,0 +1,12 @@
+package non_linear_dataStructures.binary_tree;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Tree tr = new Tree();
+        tr.populate(sc);
+        // tr.display();
+        tr.prettyDisplay();
+    }
+}
