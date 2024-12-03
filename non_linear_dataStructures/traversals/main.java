8e60cf452dfa2510275fce4757cd75c7946cda39
Sachinxmpl <sachinxmpl6@gmail.com>
2024-12-03 08:09:19 +0545

    remove

--- non_linear_dataStructures/traversals/main.java
+++
@@ -1,9 +0,0 @@
-package non_linear_dataStructures.traversals;
-
-public class main {
-    public static void main(String[] args) {
-        BinaryTree bt = new BinaryTree();
-        bt.populate();
-        bt.preOrderTraversal(bt.root);
-    }
-}
