ec96f6eabb4508337c501f7c47db891e604eecfe
Sachinxmpl <sachinxmpl6@gmail.com>
2024-12-03 08:15:32 +0545

    binary search tree

---
+++ non_linear_dataStructures/binary_search_tree/main.java
@@ -0,0 +1,17 @@
+package non_linear_dataStructures.binary_search_tree;
+
+public class main {
+    public static void main(String[] args) {
+        BinarySearchTree bt = new BinarySearchTree();
+        int[] nums = {4,8,2,5,0,9,6,3,7,1};
+        bt.populate(nums);
+        bt.display();
+
+        System.out.println("-----------------");
+        BinarySearchTree soted = new BinarySearchTree();
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        soted.populateSorted(arr);
+        soted.display();
+        
+    }
+}
