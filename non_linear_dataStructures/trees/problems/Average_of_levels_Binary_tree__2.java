6070929b69aaa19c93355905c48a2a4fca44e717
Sachinxmpl <sachinxmpl6@gmail.com>
2024-12-09 08:16:31 +0545

    add problem

---
+++ non_linear_dataStructures/trees/problems/Average_of_levels_Binary_tree__2.java
@@ -0,0 +1,57 @@
+package Data_Structures_and_algorithms.non_linear_dataStructures.trees.problems;
+
+import java.util.*;
+
+public class Average_of_levels_Binary_tree__2 {
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<Double>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
+
+        while (!que.isEmpty()) {
+            int levelSize = que.size();
+            double sum = 0;
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = que.poll();
+                sum = sum + currNode.val;
+                if (currNode.left != null) {
+                    que.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    que.add(currNode.right);
+                }
+            }
+            double avg = sum / (levelSize);
+            result.add(avg);
+        }
+
+        return result;
+    }
+}
