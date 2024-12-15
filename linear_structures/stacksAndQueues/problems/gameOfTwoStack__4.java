382861a66b286d5ef851251a955617bdf8367b12
Sachinxmpl <sachinxmpl6@gmail.com>
2024-12-15 09:29:39 +0545

    game of two stacks

--- linear_structures/stacksAndQueues/problems/gameOfTwoStack__4.java
+++ linear_structures/stacksAndQueues/problems/gameOfTwoStack__4.java
@@ -16,36 +16,44 @@ public class gameOfTwoStack__4 {
             int m = sc.nextInt();
             int x = sc.nextInt();
 
-            int[] a = new int[n];
-            int[] b = new int[m];
+            List<Integer> l1 = new ArrayList<>();
+            List<Integer> l2 = new ArrayList<>();
             for (int j = 0; j < n; j++) {
-                a[j] = sc.nextInt();
+                int temp = sc.nextInt();
+                l1.add(temp) ; 
             }
             for (int j = 0; j < m; j++) {
-                b[j] = sc.nextInt();
+                int temp = sc.nextInt() ; 
+                l2.add(temp) ; 
             }
 
-            System.out.println(gamer(a, b, x));
+            System.out.println(twoStacks(x  , l1 , l2));
         }
         sc.close();
     }
 
-    static int gamer(int[] a, int[] b, int sum) {
-        return gamer(a, b, sum, 0, 0);
+    public static int twoStacks(int maxSum, List<Integer> a, List<Integer> b) {
+        return helper(a, b, maxSum, 0, 0);
     }
 
-    static int gamer(int[] a, int[] b, int sum, int currSum, int count) {
-        if (currSum > sum) {
+    static int helper(List<Integer> a, List<Integer> b, int maxSum, int currSum, int count) {
+        if (currSum > maxSum) {
+            return count - 1;
+        }
+        if (a.isEmpty() && b.isEmpty()) {
             return count;
         }
-        if (a.length == 0 || b.length == 0) {
-            return count;
+        if (a.isEmpty()) {
+            return helper(a, b.subList(1, b.size()), maxSum, currSum + b.get(0), count + 1);
         }
-
-        int ans1 = gamer(Arrays.copyOfRange(a, 1, a.length), b, sum, currSum + a[0], count + 1);
-        int ans2 = gamer(a, Arrays.copyOfRange(b, 1, b.length), sum, currSum + b[0], count + 1);
-
-        return Math.max(ans1, ans2) - 1 ;
+        if (b.isEmpty()) {
+            return helper(a.subList(1, a.size()), b, maxSum, currSum + a.get(0), count + 1);
+        }
+        List<Integer> newList1 = a.subList(1, a.size());
+        List<Integer> newList2 = b.subList(1, b.size());
+        int left = helper(newList1, b, maxSum, currSum + a.get(0), count + 1);
+        int right = helper(a, newList2, maxSum, currSum + b.get(0), count + 1);
+        return Math.max(left, right);
     }
 
 }
